1eb928e6c48cc0b25e00a57cfebd0169ed5c3b01
Victor Bozelli Alvarez <vbozelli10@gmail.com>
2024-08-13 10:02:13 -0700

    Revert "Revert "Replaced the dependency html2canvas with the dependency dom-to-image-more""
    This reverts commit 0fed8e14bea3060d5866bd7d102fa4b01cbb48d8.

--- src/RNViewShot.web.js
+++ src/RNViewShot.web.js
@@ -1,29 +1,15 @@
 //@flow
-import html2canvas from "html2canvas";
+import domtoimage from 'dom-to-image-more';
 
 async function captureRef(view, options) {
   if (options.result === "tmpfile") {
     console.warn("Tmpfile is not implemented for web. Try base64 or file.\n"+
                  "For compatibility, it currently returns the same result as data-uri");
   }
-
-  // TODO: implement snapshotContentContainer option
-
-  const h2cOptions = {};
-  let renderedCanvas = await html2canvas(view, h2cOptions);
-
-  if (options.width && options.height) {
-    // Resize result
-    const resizedCanvas = document.createElement('canvas');
-    const resizedContext = resizedCanvas.getContext('2d');
-    resizedCanvas.height = options.height;
-    resizedCanvas.width = options.width;
-    resizedContext.drawImage(renderedCanvas, 0, 0, resizedCanvas.width, resizedCanvas.height);
-    renderedCanvas = resizedCanvas;
+  const dataUrl = await domtoimage.toSvg(view);
+  if (options.result === "data-uri" || options.result === "tmpfile") {
+    return dataUrl;
   }
-
-  const dataUrl = renderedCanvas.toDataURL("image/" + options.format, options.quality);
-  if (options.result === "data-uri" || options.result === "tmpfile") return dataUrl;
   return dataUrl.replace(/data:image\/(\w+);base64,/, '');
 }
 
